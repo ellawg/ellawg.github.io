d1b0e7e62f554638f5ee8ef0d5d180fab6626d50
Ella Wirstad <ella.wg@hotmail.com>
2019-04-05 16:17:37 +0200

    add card & list components

---
+++ src/components/PracticeCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="practice-card">
+    <v-ons-card>
+      <div class="title">{{card.name}}</div>
+    </v-ons-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PracticeCard',
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/PracticeList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="practice">
+    <v-ons-page>
+      <PracticeCard/>
+    </v-ons-page>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PracticeList',
+  components: PracticeCard,
+  data(){
+    return {
+      practice1: {name: 'p1', id: 1},
+      practice2: {name: 'p2', id: 2},
+      practice3: {name: 'p3', id: 3}
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
