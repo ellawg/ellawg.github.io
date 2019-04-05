e40b2a27fb834184ca1d5feaf7a31282c812a80f
Ella Wirstad <ella.wg@hotmail.com>
2019-04-05 15:22:03 +0200

    init vue + onsen

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+// Webpack CSS import
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+// JS import
+import VueOnsen from 'vue-onsenui'; // This imports 'onsenui', so no need to import it separately
+
+Vue.use(VueOnsen); // VueOnsen set here as plugin to VUE. Done automatically if a call to window.Vue exists in the startup code.
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
