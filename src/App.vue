f24fd70596db9afddeacba3a6c1b061bca686f97
Ella Wirstad <ella.wg@hotmail.com>
2019-04-05 15:54:28 +0200

    add spashscreen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to your relaxation app"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SplashScreen from './components/SplashScreen.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SplashScreen
   }
 }
 </script>
